6c82f3a70432127239c41448466aa507138e5a73
Bionic_Scout <roman.higginson@zoominternet.net>
2021-10-19 11:47:45 -0400

    Restart
    Restart button added  ('r' key). Jump pad introduced but not finished

--- Assets/Scripts/PlayerCollition.cs
+++ Assets/Scripts/PlayerCollition.cs
@@ -6,6 +6,13 @@ public class PlayerCollition : MonoBehaviour
 {
 
     public PlayerMovement movement;
+    public GameObject player;
+    Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = player.transform.position;
+    }
 
     void OnCollisionEnter(Collision collisionInfo)
     {
@@ -13,5 +20,21 @@ public class PlayerCollition : MonoBehaviour
         {
             movement.enabled = false; 
         }
+
+        if (collisionInfo.collider.tag == "Jump Pad")
+        {
+            movement.SetJumpTicks();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonUp("Restart"))
+        {
+            player.transform.position = startPos;
+            player.transform.Rotate(0.0f, 0.0f, 0.0f, Space.Self);
+
+            movement.enabled = true;
+        }     
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,11 @@ public class PlayerMovement : MonoBehaviour
     public float Speed = 25f;
     public float PlayerMotion = 25f;
 
+    int jumpTicks = 0;
+
     // Update is called once per frame
     void FixedUpdate()
     {
-
         rb.AddForce(-Speed, 0, 0 * Time.deltaTime);
 
         if (Input.GetKey("d"))
@@ -23,5 +24,16 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(0, 0, -PlayerMotion * Time.deltaTime, ForceMode.VelocityChange);
         }
+
+        if (jumpTicks > 0)
+        {
+            rb.AddForce(0f, PlayerMotion * Time.deltaTime, 0f, ForceMode.VelocityChange);
+            jumpTicks -= 1;
+        }
+    }
+
+    public void SetJumpTicks()
+    {
+        jumpTicks = 100;
     }
 }
