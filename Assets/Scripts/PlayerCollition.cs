253ef73d362d9e33f9407ee480442bbc8af278ff
Bionic_Scout <roman.higginson@zoominternet.net>
2021-10-26 11:30:12 -0400

    test

--- Assets/Scripts/PlayerCollition.cs
+++ Assets/Scripts/PlayerCollition.cs
@@ -6,12 +6,105 @@ public class PlayerCollition : MonoBehaviour
 {
 
     public PlayerMovement movement;
+    public GameObject player;
+    Vector3 startPos;
+
+    public GameObject shieldPrefab;
+    GameObject shield;
+
+    List<GameObject> moved = new List<GameObject>();
+
+    private void Start()
+    {
+        startPos = player.transform.position;
+    }
 
     void OnCollisionEnter(Collision collisionInfo)
     {
+
+        if (collisionInfo.gameObject.CompareTag("ShieldPickUp"))
+        {
+            //Move PickUp Below Level
+            Vector3 temp = collisionInfo.gameObject.transform.position;
+            collisionInfo.gameObject.transform.position = new Vector3(temp.x, temp.y - 5, temp.z);
+
+            moved.Add(collisionInfo.gameObject);
+
+            //Add Shield
+            temp = player.transform.position;
+            shield = Instantiate(shieldPrefab, new Vector3(temp.x, 0, temp.z), Quaternion.identity);
+            shield.transform.parent = player.transform;
+
+            movement.setShield(true);
+            Debug.Log("Got Shield");
+        }
+
         if (collisionInfo.collider.tag == "Obstical")
         {
-            movement.enabled = false; 
+            if (movement.getShield()) //If Player Has Shiled they keep going
+            {
+                //Move obsticle below scene
+                Vector3 temp = collisionInfo.gameObject.transform.position;
+                collisionInfo.gameObject.transform.position = new Vector3(temp.x, temp.y - 5, temp.z);
+                Debug.Log("Protected");
+
+                moved.Add(collisionInfo.gameObject);
+
+                //Deal with Shield
+                Destroy(shield);
+                movement.setShield(false);
+            }
+            else
+            { //else Fail
+                movement.enabled = false;
+                Debug.Log("Dead");
+
+                restart();
+            }
         }
+
+        if (collisionInfo.gameObject.CompareTag("JumpPad"))
+        {
+            movement.Jump();
+            Debug.Log("Jump");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonUp("Restart"))
+        {
+            restart();
+            Debug.Log("Restart");
+        }
+
+        if (player.transform.position.y < -5)
+        {
+            restart();
+            Debug.Log("Fell Off");
+        }
+    }
+
+    void restart()
+    {
+
+        player.transform.position = startPos;
+        player.transform.Rotate(0.0f, 0.0f, 0.0f, Space.Self);
+
+        Debug.Log("Restart");
+        for (int i = 0; i < moved.Count; i++)
+        {
+            Vector3 temp = moved[i].transform.position;
+            moved[i].transform.position = new Vector3(temp.x, temp.y + 5, temp.z);
+            Debug.Log("Protected");
+        }
+
+        Destroy(shield);
+        movement.setShield(false);
+
+        moved.Clear();
+
+        movement.Restart();
+        movement.enabled = true;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,25 @@ public class PlayerMovement : MonoBehaviour
     public float Speed = 25f;
     public float PlayerMotion = 25f;
 
+    public bool hasShield = false;
+
+    int jumpTicks = 0;
+    public int maxJump = 0;
+
+    private void Update()
+    {
+        if (jumpTicks > 0)
+        {
+            rb.AddForce(0, PlayerMotion * Time.deltaTime, 0, ForceMode.VelocityChange);
+            jumpTicks--;
+
+            Debug.Log("jump");
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-
         rb.AddForce(-Speed, 0, 0 * Time.deltaTime);
 
         if (Input.GetKey("d"))
@@ -23,5 +38,42 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(0, 0, -PlayerMotion * Time.deltaTime, ForceMode.VelocityChange);
         }
+
+
+        //Clamp
+        if (rb.velocity.x < -50) //Forward Movement
+        {
+            rb.velocity = new Vector3(-50, rb.velocity.y, rb.velocity.z);
+        }
+
+        if (rb.velocity.z < -25) //Left Movement
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -25);
+        }
+
+        if (rb.velocity.z > 25) //Right Movement
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 25);
+        }
+    }
+
+    public void Jump()
+    {
+        jumpTicks = maxJump;
+    }
+
+    public void Restart()
+    {
+        rb.velocity = Vector3.zero;
+    }
+
+    public void setShield(bool shield)
+    {
+        hasShield = shield;
+    }
+
+    public bool getShield()
+    {
+        return hasShield;
     }
 }
